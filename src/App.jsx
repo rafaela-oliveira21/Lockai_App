961cebe09a2ca4429cf90208d16ef7ed436d751b
rafalela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-09-01 20:03:45 -0300

    insert cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import RecuperarSenha from "./pages/RecuperarSenha";
 import Cadastro from "./pages/cadastro";
+import CodigoSenha from "./pages/CodigoSenha";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/recuperar-senha" element={<RecuperarSenha />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/CodigoSenha" element={<CodigoSenha/>}/>
       </Routes>
     </Router>
   );
