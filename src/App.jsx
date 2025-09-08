3709acefd1c8443ca84d4b560608926118c777df
rafalela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-09-08 20:35:08 -0300

    style: removed pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
-import Home from "./pages/Home";
 import RecuperarSenha from "./pages/RecuperarSenha";
 import AlterarSenha from "./pages/AlterarSenha";
 import CodigoSenha from "./pages/CodigoSenha";
-import CadastroPlanos from "./pages/CadastroPlanos";
-import CadastroPlanosCategoria from "./pages/CadastroPlanosCategoria";
 import Pagamento from "./pages/Pagamento";
 
 
@@ -15,14 +12,11 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/Login" element={<Login />} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
         <Route path="/CodigoSenha" element={<CodigoSenha/>}/>
         <Route path="/AlterarSenha" element={<AlterarSenha />}/>
         <Route path="/Pagamento" element={<Pagamento/>}/>
-        <Route path="/Home" element={<Home/>}/>
-        <Route path="/CadastroPlanos" element={<CadastroPlanos/>}/>
-        <Route path="/CadastroPlanosCategoria" element={<CadastroPlanosCategoria/>}/>
       </Routes>
     </Router>
   );
